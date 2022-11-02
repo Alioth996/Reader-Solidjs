6006ab72786bafe8e56452e314e62c9aa08254eb
alex <2844520415@qq.com>
2022-11-02 23:37:43 +0800

    feat: 添加作者和目录正则

--- src/util/parseBook.ts
+++ src/util/parseBook.ts
@@ -1,5 +1,6 @@
 interface BookInfo {
   author: string
+  localPath: string
 }
 
 /**
@@ -9,7 +10,26 @@ interface BookInfo {
  */
 
 const parseBook = book => {
+  let author = ''
+
+  // 章节数组: 存放所有章节
+  let catalogList = ['']
+
   //   console.log(book)
+
+  // 作者正则
+  const authorRegx = new RegExp(/[作者:]?\W/g)
+
+  /**
+   *  (\s|\n) : 章节名以空格或换行符开始
+   * (第) ： 章节名第一个字为第
+   * ([\u4e00-\u9fa5a-zA-Z0-9]{1,7}) ：第xx章 中的xx,这里xx表示匹配1-7中文英文和数字，如‘ 第一千五百三十六章：茅山现状 ’
+   * [章]： 章节名中的章
+   * [^\n]{1,35} ：表示匹配章后面1-35个非换行符的章节名字
+   * (|\n) ：表示以换行符结尾
+   * @参考资料 https://blog.csdn.net/qq_43257319/article/details/108530208
+   */
+  const catalogRegx = new RegExp(/(\s|\n)(第)?([\u4e00-\u9fa5a-zA-Z0-9]{1,7})[章|卷|部|回|节|集]?[^\n]{1,35}(|\n)/g)
 }
 
 export default parseBook
