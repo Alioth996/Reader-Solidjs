6e9bccfe104aa09a7ff92659e8010ea93f315255
alex <2844520415@qq.com>
2022-11-03 23:48:52 +0800

    chore: 新增阅读器界面

--- src/views/Reader/index.tsx
+++ src/views/Reader/index.tsx
@@ -1,15 +1,50 @@
-import { Component } from 'solid-js'
-import AppTitle from '../../components/AppTitle'
-import NavBar from '../../components/NavBar'
+import { Link } from 'solid-app-router'
+import { Component, Show, createSignal } from 'solid-js'
+import './reader.less'
+
 const Reader: Component = () => {
+  const [isbarMenuShow, setIsbarMenuShow] = createSignal(false)
+
+  const showBarAndMenu = () => {
+    setIsbarMenuShow(!isbarMenuShow())
+    // console.log(isbarMenuShow)
+  }
+
   return (
     <div id='Reader'>
-      <AppTitle text={'阅读中'} />
-      <main id='reader-container'>
+      {/* 顶部栏 */}
+      <Show when={isbarMenuShow()}>
+        <header id='top-bar'>
+          <Link href='/'>
+            <button>返回</button>
+          </Link>
+        </header>
+      </Show>
+
+      {/* 正文 */}
+      <main id='reader-container' onclick={showBarAndMenu}>
+        <div id='catelog-title'>
+          <p class='catelog'>第三章 崖高人远</p>
+        </div>
         <article></article>
       </main>
 
-      <NavBar />
+      {/* 底部菜单 */}
+      <Show when={isbarMenuShow()}>
+        {/* <div id='reader-progress'>
+          <div> {'<'}</div>
+          <div>
+            <progress value='22' max='100'></progress>
+          </div>
+          <div> {'>'}</div>
+        </div> */}
+
+        <div id='menu-nar'>
+          <div>目录</div>
+          <div>黑夜</div>
+          <div>设置</div>
+        </div>
+      </Show>
     </div>
   )
 }
