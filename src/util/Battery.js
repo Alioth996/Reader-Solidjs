3f400aba1caa5a6db668ab3d27288fd36ee1ce9d
alex <2844520415@qq.com>
2022-11-04 12:18:00 +0800

    chore: 添加getBattery方法的注释

--- src/util/Battery.js
+++ src/util/Battery.js
@@ -1,13 +1,13 @@
-//获取手机电量信息
-// charging: 是否在充电
-// chargingTime: 充满电还需要的时间(秒)
-// dischargingTime:  电池剩余可用时间(秒)
-// level: 剩余电量,最大电量就是1
-// onchargingchange:  充电状态改变时触发该监听函数
-// onchargingtimechange:  充满还需时间改变时触发该监听函数
-// ondischargingtimechange:  电池剩余可用时间改变时触发该监听函数
-// onlevelchange:  电量改变时触发该监听函数
-
+/**
+ *
+ * @desc 获取设备电量信息
+ * @return {*object} power
+ * @return  power.charging: 是否在充电
+ * @return  power.chargingTime: 充满电还需要的时间(秒)
+ * @return  power.dischargingTime:  电池剩余可用时间(秒)
+ * @return  power.level: 剩余电量,最大电量就是1
+ * @mdn https://developer.mozilla.org/zh-CN/docs/Web/API/Navigator/getBattery
+ */
 const getBattery = () => {
   let power = {
     isCharging: false,
@@ -15,11 +15,17 @@ const getBattery = () => {
     needChargingTime: '0',
     elseTime: '0'
   }
+
   navigator.getBattery().then(battery => {
     power.isCharging = battery.charging
     power.level = battery.level
     power.needChargingTime = battery.chargingTime
     power.elseTime = battery.dischargingTime
+
+    // onchargingchange: 充电状态改变时触发该监听函数
+    // onchargingtimechange: 充满还需时间改变时触发该监听函数
+    // ondischargingtimechange: 电池剩余可用时间改变时触发该监听函数
+    // onlevelchange: 电量改变时触发该监听函数
 
     battery.addEventListener('chargingchange', () => {
       power.isCharging = battery.charging
