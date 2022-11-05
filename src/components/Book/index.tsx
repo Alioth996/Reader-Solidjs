33eb07dae2705f397987b314019b0d6b070fbbd6
alex <2844520415@qq.com>
2022-11-05 11:58:42 +0800

    chore: book组件size封装,

--- src/components/Book/index.tsx
+++ src/components/Book/index.tsx
@@ -2,15 +2,13 @@ import './index.less'
 
 export default function (props) {
   const { book, size } = props
-  let bookClssList = 'book'
-  if (size) bookClssList = `book ${size}`
-
-  console.log(bookClssList)
+  // let bookClssList = 'book'
+  // if (size) bookClssList = `book ${size}`
 
   return (
-    <div class={bookClssList}>
+    <div class='book' classList={{ 'book-large': size == 'large' }}>
       <p class='book-type-tag'> {book.type} </p>
-      <p class='font-large'> {book.name} </p>
+      <p class='text-center'> {book.name} </p>
       <p style='letter-spacing:3px'>
         <b>{book.author}</b>
       </p>
