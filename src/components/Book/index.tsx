46583fecd9269ff2567e0af02ea3d6396a75bafe
alex <2844520415@qq.com>
2022-11-05 14:25:04 +0800

    chore: change book

--- src/components/Book/index.tsx
+++ src/components/Book/index.tsx
@@ -1,12 +1,12 @@
 import './index.less'
 
 export default function (props) {
-  const { book, size } = props
+  const { book = { name: '', author: '', type: 'txt' }, size } = props
   // let bookClssList = 'book'
   // if (size) bookClssList = `book ${size}`
 
   return (
-    <div class='book' classList={{ 'book-large': size == 'large' }}>
+    <div class='book' classList={{ 'book-large': size == 'large', 'book-small': size == 'small' }}>
       <p class='book-type-tag'> {book.type} </p>
       <p class='text-center'> {book.name} </p>
       <p style='letter-spacing:3px'>
